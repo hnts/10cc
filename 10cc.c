4dae0e8a9befac7fc59091fd3e2cf39dde3d2ecf
hnts <hanatsuu@gmail.com>
2020-05-02 14:09:40 +0900

    can tokenize '(' and ')'

--- 10cc.c
+++ 10cc.c
@@ -194,7 +194,7 @@ Token *tokenize(char *p) {
       continue;
     }
 
-    if (*p == '+' || *p == '-' || *p == '*' || *p == '/') {
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
